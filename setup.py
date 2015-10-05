ef8b516ad9f5a97fca4ab412d1f3cf1e9c8f288d
Ben Jeffrey <mail@benjeffrey.net>
2015-10-05 16:29:32 +0100

    Update version number and fork URL.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os, sys
 
 setup(name='dailymotion',
-      version='0.2.0',
+      version='0.3.0',
       description='Dailymotion API SDK',
       long_description='Dailymotion API SDK',
       classifiers=[
@@ -17,7 +17,7 @@ setup(name='dailymotion',
       keywords=['dailymotion', 'api', 'sdk', 'graph'],
       author='Samir AMZANI',
       author_email='samir.amzani@gmail.com',
-      url='http://github.com/dailymotion/dailymotion-sdk-python',
+      url='http://github.com/boilerroomtv/dailymotion-sdk-python',
       license='Apache License, Version 2.0',
       include_package_data=True,
       zip_safe=False,
